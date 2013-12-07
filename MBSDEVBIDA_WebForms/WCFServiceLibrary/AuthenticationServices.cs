fe37e02414a7baa9d5d9a5a38d59f904360f7948
nolsen7187 <nolsen@regis.edu>
2013-12-06 18:45:45 -0700

    WCFServiceAdded and host, both are working to authenticate.

---
+++ MBSDEVBIDA_WebForms/WCFServiceLibrary/AuthenticationServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using SVC;
+
+namespace WCFServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class AuthenticationServices : IServices
+    {
+        private bool logonFound, passwordFound;
+
+        public bool AuthenticateUser(string logon, string password)
+        {
+            SVC_AuthenticateUser svcAuthenticateUser = new SVC_AuthenticateUser();
+            logonFound = svcAuthenticateUser.AuthenticateLogon(logon);
+            passwordFound = svcAuthenticateUser.AuthenticatePassword(password);
+
+            if ((logonFound && passwordFound) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
